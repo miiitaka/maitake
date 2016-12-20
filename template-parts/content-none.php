e9c458df71f57072b124dec7f8c6bc450e4a743d
Kazuya Takami <foundationmeister@outlook.com>
2016-12-20 16:17:20 +0900

    404.php updated.

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -11,16 +11,19 @@
 	<header>
 		<h1>Nothing Found</h1>
 	</header>
-
-	<div>
+	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 			<p><?php printf( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 		<?php elseif ( is_search() ) : ?>
 			<p>'Sorry, but nothing matched your search terms. Please try again with some different keywords.</p>
-			<?php get_search_form(); ?>
+			<div class="page-404-search">
+				<?php get_search_form(); ?>
+			</div>
 		<?php else : ?>
 			<p>It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.</p>
-			<?php get_search_form(); ?>
+			<div class="page-404-search">
+				<?php get_search_form(); ?>
+			</div>
 		<?php endif; ?>
 	</div>
 </section>
