c2a06d4707cbdd99a5a2ef367fff0d0c57589d8e
Kazuya Takami <foundationmeister@outlook.com>
2017-01-18 17:44:07 +0900

    Delete alternate.

--- header.php
+++ header.php
@@ -11,7 +11,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="alternate" hreflang="<?php bloginfo( 'language' ) ?>" href="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
