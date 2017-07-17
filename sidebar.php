c718dd1f9a88f4578b604a1f6b3061cdf09c3643
Kazuya Takami <foundationmeister@outlook.com>
2017-07-17 16:57:56 +0900

    Added Hook point.

--- sidebar.php
+++ sidebar.php
@@ -10,6 +10,8 @@
 
 <?php if ( is_active_sidebar( 'sidebar-1' )  ) : ?>
 	<aside class="layout-sidebar">
+		<?php do_action( 'layout-sidebar-top-hook' ); ?>
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+		<?php do_action( 'layout-sidebar-bottom-hook' ); ?>
 	</aside>
 <?php endif; ?>
