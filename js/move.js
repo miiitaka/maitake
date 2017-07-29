561ebdcb3de74145c9d31fb2663a4720b014320b
Kazuya Takami <foundationmeister@outlook.com>
2017-07-29 17:42:46 +0900

    Updated.

--- js/move.js
+++ js/move.js
@@ -1,13 +1,35 @@
-(function($) {
-	"use strict";
-	$(function() {
+;(function($){
+	$.fn.topMove = function(options){
+		var defaults = {
+			"speed"  : 500,
+			"scroll" : 300
+		};
 		var
-			event = "ontouchend" in window ? "touchend" : "click",
-			elmMove = $("#move-to-the-top");
+			settings = $.extend( {}, defaults, options ),
+			event = "ontouchend" in window ? "touchend" : "click";
 
-		elmMove.on(event, function(e) {
-			e.preventDefault();
-			$('html, body').animate({scrollTop: 0}, 500);
+		return this.each(function(){
+			var self = $(this);
+			self.hide();
+
+			self.on(event, function(e) {
+				e.preventDefault();
+				$('html, body').animate({scrollTop: 0}, settings.speed);
+			});
+
+			$(window).on('scroll', function () {
+				if ($(this).scrollTop() > settings.scroll) {
+					self.fadeIn();
+				} else {
+					self.fadeOut();
+				}
+			});
 		});
-	});
-})(jQuery);
+	};
+
+	var param = {
+		speed: 500,
+		scroll: 300
+	};
+	$("#move-to-the-top").topMove(param);
+}(jQuery));
