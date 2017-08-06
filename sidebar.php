9aaa96fb7e624d0044956b9be218bff707e7cd2f
Kazuya Takami <foundationmeister@outlook.com>
2017-08-06 18:07:01 +0900

    Updated.

--- sidebar.php
+++ sidebar.php
@@ -9,9 +9,9 @@
 ?>
 
 <?php if ( is_active_sidebar( 'sidebar-1' )  ) : ?>
-	<aside class="layout-sidebar">
+	<div class="layout-sidebar">
 		<?php do_action( 'layout-sidebar-top-hook' ); ?>
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 		<?php do_action( 'layout-sidebar-bottom-hook' ); ?>
-	</aside>
+	</div>
 <?php endif; ?>
