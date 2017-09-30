2f70beedd44dab1c60a5ae67ed25580178433214
Kazuya Takami <foundationmeister@outlook.com>
2017-09-30 16:49:40 +0900

    oEmbed Updated.

--- functions.php
+++ functions.php
@@ -154,11 +154,6 @@ function theme_remove_action_head() {
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 	remove_action( 'wp_print_styles',     'print_emoji_styles' );
 	remove_action( 'admin_print_styles',  'print_emoji_styles' );
-
-	// Remove oEmbed.
-	remove_action( 'wp_head', 'rest_output_link_wp_head' );
-	remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
-	remove_action( 'wp_head', 'wp_oembed_add_host_js' );
 }
 add_action( 'after_setup_theme', 'theme_setup' );
 
